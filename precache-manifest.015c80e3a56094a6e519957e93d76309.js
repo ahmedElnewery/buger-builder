83ddee4687d4127f24a83a9a9e1fedfd88f1df00
ahmed mahmoud <ahmedm.elnewery@gmail.com>
2021-03-30 15:05:49 +0200

    Updates

---
+++ precache-manifest.015c80e3a56094a6e519957e93d76309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4360479ebdd38da5a84d56c1f09ae38f",
+    "url": "/ahmedElnewery/buger-builder/index.html"
+  },
+  {
+    "revision": "899542938d7d4ee30683",
+    "url": "/ahmedElnewery/buger-builder/static/css/main.78a4033d.chunk.css"
+  },
+  {
+    "revision": "d7bfcd6980f700ba56b1",
+    "url": "/ahmedElnewery/buger-builder/static/js/2.79b8b913.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/ahmedElnewery/buger-builder/static/js/2.79b8b913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899542938d7d4ee30683",
+    "url": "/ahmedElnewery/buger-builder/static/js/main.011c5606.chunk.js"
+  },
+  {
+    "revision": "6cef047e68c1db21079c",
+    "url": "/ahmedElnewery/buger-builder/static/js/runtime-main.a872babb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/ahmedElnewery/buger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
